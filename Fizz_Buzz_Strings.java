2e9487b2ba68d652038260fe50a0dc855715bf57
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-05-03 16:49:51 +0530

    Add files via upload

---
+++ Fizz_Buzz_Strings.java
@@ -0,0 +1,12 @@
+public class Fizz_Buzz_Strings {
+        public List<String> fizzBuzz(int n) {
+            List<String> answer = new ArrayList<>();
+            for(int i=1;i<=n;i++){
+            answer.add(
+                 i % 15 == 0 ? "FizzBuzz" :
+                 i % 3 == 0 ? "Fizz" :
+                 i % 5 == 0 ? "Buzz" : String.valueOf(i) );
+            }
+            return answer;
+        }
+}
