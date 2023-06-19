8dd363c0318290abb81790d794830ac8f5434b13
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-06-19 14:41:29 +0530

    Add files via upload

---
+++ FindTheHighestAltitude.java
@@ -0,0 +1,11 @@
+public class FindTheHighestAltitude {
+        public int largestAltitude(int[] gain) {
+            int max = 0;
+            int current = 0;
+            for (int i = 0; i < gain.length; i++) {
+                current += gain[i];
+                max = Math.max(max, current);
+            } 
+            return max;
+        }
+    }
