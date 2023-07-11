bedfd20843de7ed63f21d6e90bf110e65b90c0ed
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-07-12 00:14:53 +0530

    Add files via upload

---
+++ AllNodesDistanceKinBinaryTree.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class AllNodesDistanceKinBinaryTree {
+    public void getAllNodesFromK(TreeNode root,int k,List<Integer> res){
+        if(root==null){
+            return ;
+        }
+        if(k==0){
+            res.add(root.val);
+            return ;
+        }
+        getAllNodesFromK(root.left,k-1,res);
+        getAllNodesFromK(root.right,k-1,res);
+
+         
+    }
+    public int solve(TreeNode root, TreeNode target, int k,List<Integer> res){
+        if(root==null){
+            return -1;
+        }
+        if(root==target){
+            getAllNodesFromK(root,k,res);
+            return 1;
+        }
+        int left = solve(root.left,target,k,res);
+        int right = solve(root.right,target,k,res);
+        if(left!=-1 && left<=k){
+            if(left==k){
+                res.add(root.val);
+            }
+            else{
+                getAllNodesFromK(root.right,k-left-1,res);
+            }
+            return 1+left;
+        }
+        if(right!=-1 && right<=k){
+            if(right==k){
+                res.add(root.val);
+            }
+            else{
+                getAllNodesFromK(root.left,k-right-1,res);
+            }
+            return 1+right;
+        }
+        return -1;
+
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> res=new ArrayList<>();
+        solve(root,target,k,res);
+        return res;
+    }
+}
