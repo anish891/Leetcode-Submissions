99d106bdb069c4b080bfab2a2b7d2ed7aaa11337
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-07-20 06:14:38 +0530

    Add files via upload

---
+++ AsteroidCollision.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] a) {
+        int top = -1;
+        for (int x : a) {
+            boolean stillAlive = true;
+            while (stillAlive && x < 0 && top >= 0 && a[top] > 0) {
+                stillAlive = a[top] + x < 0;
+                if (a[top] + x <= 0) top--;
+            }
+            if (stillAlive) a[++top] = x;
+        }
+        return Arrays.copyOf(a, top+1);
+    }
+}
