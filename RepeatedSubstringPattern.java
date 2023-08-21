2cc8ce0d0eddf1db794c1ba372a45041b0772ef1
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-08-21 19:26:54 +0530

    Add files via upload

---
+++ RepeatedSubstringPattern.java
@@ -0,0 +1,17 @@
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+        
+        //abcabcabc
+        for(int i = len / 2; i > 0; i--){
+            if(len % i == 0 && s.charAt(i - 1) == s.charAt(len - 1)){
+                String ss = s.substring(0, i);
+                int repeat = len / i;
+                if(ss.repeat(repeat).equals(s)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
