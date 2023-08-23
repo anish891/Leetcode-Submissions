7004964c9a21b142ae817aee7503c6a096a4d9af
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-08-23 13:02:27 +0530

    Add files via upload

---
+++ UnivaluedBinaryTree.java
@@ -0,0 +1,17 @@
+import javax.swing.tree.TreeNode;
+
+public class UnivaluedBinaryTree {
+    public boolean isUnivalTree(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+
+        if (root.left != null && root.left.val != root.val)
+            return false;
+        if (root.right != null && root.right.val != root.val)
+            return false;
+
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+        
+    }
+}
