77f4ddac8c4edef193b094a86016c553eeb301f8
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-10-12 01:08:21 +0530

    Add files via upload

---
+++ MajorityElement(UsingHashMap).java
@@ -0,0 +1,17 @@
+public class MajorityElement(UsingHashMap) {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+
+            //ShortHand
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        for(Integer key : map.keySet()){
+            if(map.get(key) > nums.length/2){
+                return key;
+            }
+        }
+        return 0;
+    }
+}
