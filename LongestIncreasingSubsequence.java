4850f3fd84cd6884379b69d6f0f1d66b7756d140
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2023-12-19 17:14:16 +0530

    Add files via upload

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LongestIncreasingSubsequence {
+     public int lengthOfLIS(int[] nums) {
+        List<Integer> sub = new ArrayList<>();
+
+        for (int num : nums) {
+            int i = Collections.binarySearch(sub, num);
+
+            if (i < 0) {
+                i = -(i + 1);
+            }
+
+            if (i == sub.size()) {
+                sub.add(num);
+            } else {
+                sub.set(i, num);
+            }
+        }
+        
+        return sub.size();
+    }
+}
