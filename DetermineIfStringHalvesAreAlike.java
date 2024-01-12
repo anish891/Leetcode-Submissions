fadbdb131c290ebd4fd38bd6199857c6cc62ae1e
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-01-12 16:37:49 +0530

    Add files via upload

---
+++ DetermineIfStringHalvesAreAlike.java
@@ -0,0 +1,22 @@
+public class DetermineIfStringHalvesAreAlike {
+        public boolean halvesAreAlike(String s) {
+            int mid = s.length() / 2;
+            String firstHalf = s.substring(0, mid);
+            String secondHalf = s.substring(mid);
+            
+            int countFirst = countVowels(firstHalf);
+            int countSecond = countVowels(secondHalf);
+            
+            return countFirst == countSecond;
+        }
+        
+        private int countVowels(String str) {
+            int count = 0;
+            for (char ch : str.toCharArray()) {
+                if ("AEIOUaeiou".indexOf(ch) != -1) {
+                    count++;
+                }
+            }
+            return count;
+        }
+}
