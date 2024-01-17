bc7a98d85179dc91593c8c8f777a4664ecbe68d1
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-01-18 01:13:30 +0530

    Add files via upload

---
+++ UniqueNumberOfOccurences.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class UniqueNumberOfOccurences {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++){
+            if (hashMap.containsKey(arr[i])){
+                hashMap.put(arr[i], hashMap.get(arr[i]) + 1);
+            }
+            else{
+                hashMap.put(arr[i], 1);
+            }
+        }
+        HashSet<Integer> hashSet = new HashSet<>();
+        boolean flag = true;
+        for (Map.Entry<Integer, Integer> mapElement : hashMap.entrySet()){
+            if (!hashSet.add(mapElement.getValue())){
+                flag = false;
+            }
+        }
+        return flag;
+    }
+}
