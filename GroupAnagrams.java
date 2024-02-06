504bb17361ec2e59a1f198bb4bf0a85157d4a1aa
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-02-06 19:12:20 +0530

    Add files via upload

---
+++ GroupAnagrams.java
@@ -0,0 +1,17 @@
+public class GroupAnagrams {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> ans = new HashMap<>();
+            for(String word : strs){
+                char[] charArray = word.toCharArray();
+                Arrays.sort(charArray);
+                String key = new String(charArray);
+    
+                if(!ans.containsKey(key)) {
+                    ans.put(key, new ArrayList<>(List.of(word)));
+                } else{
+                    ans.get(key).add(word);
+                }
+            }
+            return new ArrayList<>(ans.values());
+        }
+}
