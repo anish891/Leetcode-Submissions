c6a0ab10b78d7853eeac2cd0200197797c178c70
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-04-19 01:09:57 +0530

    Add files via upload

---
+++ IslandPerimeter.java
@@ -0,0 +1,29 @@
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int height = grid.length;
+        int width = grid[0].length;
+
+        int result = 0;
+
+        for (int y = 0; y < height; y++){
+            int prev = 0;
+            for (int x = 0; x < width; x++){
+                int keep = grid[y][x];
+                result += prev ^ keep;
+                prev = keep;
+            }
+            result += prev;
+        }
+        
+        for (int x = 0; x < width; x++){
+            int prev = 0;
+            for (int y = 0; y < height; y++){
+                int keep = grid[y][x];
+                result += prev ^ keep;
+                prev = keep;
+            }
+            result += prev;
+        }
+        return result;
+    }
+}
