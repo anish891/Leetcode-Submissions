050b7cc0d845819bd3b4b2b11b0a18ec9aa1b753
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-04-30 04:27:08 +0530

    Update CanPlaceFlowers.java

--- CanPlaceFlowers.java
+++ CanPlaceFlowers.java
@@ -1,13 +1,15 @@
 public class CanPlaceFlowers {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n==0) return true;
-        for (int i=0;i<flowerbed.length;i++){
-            if(flowerbed[i]==0 && (i==0||flowerbed[i-1]==0)&&(i==flowerbed.length-1 || flowerbed[i+1]==0)){
-                n--;
-                if (n==0) return true;
-                flowerbed[i]=1;
+        int x = flowerbed.length;
+        for(int i=0;i<x;i+=2){
+            if(flowerbed[i] == 0){
+                if(i == x-1 || flowerbed[i] == flowerbed[i+1]){
+                    n--;
+                } else {
+                    i++;
+                }
             }
         }
-        return false;
+        return n<=0;
     }
 }
