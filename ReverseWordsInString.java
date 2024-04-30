178be453c6fbbbd2a46fa840f3f3b994cbbf727a
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-05-01 03:30:55 +0530

    Update ReverseWordsInString.java

--- ReverseWordsInString.java
+++ ReverseWordsInString.java
@@ -1,23 +1,24 @@
 public class ReverseWordsInString {
-    public static String reverseWords(String s) {
-        char[] a = s.toCharArray();
-        char[] res = new char[a.length];
-        int i = a.length - 1, j, k, r = 0;
-
-        while (i >= 0 && a[i] == ' ') i--;
-
-        while (i >= 0) {
-            j = i; // end word
-
-            while (i >= 0 && a[i] != ' ') i--; // find end
-            if (r > 0) res[r++] = ' ';  // add ' ' to res[]
-
-            // reverse word and copy
-            for (k = i + 1; k <= j; k++) res[r++] = a[k];  
-
-            // clear excess space
-            while (i >= 0 && a[i] == ' ') i--; 
+    public String reverseWords(String s) {
+        char[] str  = s.toCharArray();
+        char[] res = new char[s.length()+1];
+        int l =  s.length() - 1;
+        int j;
+        int k = 0;
+        while(l>=0){
+            while(l>=0 && str[l] == ' '){
+                l--;
+            }
+            if(l<0) break;
+            j = l;
+            while(l>=0 && str[l]!=' '){
+                l--;
+            }
+            for(int i = l+1;i<=j;i++){
+                res[k++] = str[i];
+            }
+            res[k++] = ' ';
         }
-        return new String(res, 0, r);
+        return new String(res, 0 ,k-1);
     }
 }
