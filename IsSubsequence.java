f7a7f1ff6d4ea5bfcc90eadc33c5aa6ea7819626
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-05-02 01:18:15 +0530

    Update IsSubsequence.java

--- IsSubsequence.java
+++ IsSubsequence.java
@@ -1,12 +1,15 @@
 public class IsSubsequence {
         public boolean isSubsequence(String s, String t) {
-            int i = 0, j = 0;
-            while (i < s.length() && j < t.length()) {
-                if (s.charAt(i) == t.charAt(j)) {
-                    i++;
-                }
-                j++;
+        char[] m = s.toCharArray();
+        char[] n = t.toCharArray();
+        int i = 0;
+        int cnt = 0;
+        for(int j = 0; j < n.length && i< m.length; j++) {
+            if(m[i] == n[j]) {
+                i++;
+                cnt++;
             }
-            return i == s.length();
         }
+        return cnt == m.length;
+    }
 }
