073d9f1c146a39e05f12193654126c840ddf139e
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-05-04 01:57:17 +0530

    Update LongestSubarrayof1sAfterDeletingOneElement.java

--- LongestSubarrayof1sAfterDeletingOneElement.java
+++ LongestSubarrayof1sAfterDeletingOneElement.java
@@ -1,28 +1,21 @@
 public class LongestSubarrayof1sAfterDeletingOneElement {
     public int longestSubarray(int[] nums) {
-        int prevZeroPos = -1;
-        int currZeroPos = findNextZeroPosition(0, nums);
-        if (currZeroPos == nums.length) return nums.length - 1;
-
-        int longestSeq = 0;
-        while (currZeroPos != nums.length) {
-            int nextZeroPos = findNextZeroPosition(currZeroPos + 1, nums);
-            int seq = (currZeroPos - prevZeroPos - 1) + (nextZeroPos - currZeroPos - 1);
-            if (seq > longestSeq) {
-                longestSeq = seq;
-            }
-            prevZeroPos = currZeroPos;
-            currZeroPos = nextZeroPos;
-        }
-        return longestSeq;
-    }
-
-    int findNextZeroPosition(int from, int[] arr) {
-        for (int i = from; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                return i;
+        int prevWindow = 0;
+        int currWindow = 0;
+        int max_length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                max_length = Math.max(max_length, prevWindow + currWindow);
+                prevWindow = currWindow;
+                currWindow = 0;
+            } else {
+                currWindow++;
             }
         }
-        return arr.length;
+        if (currWindow == nums.length) {
+            return currWindow - 1;
+        }
+        max_length = Math.max(max_length, prevWindow + currWindow);
+        return max_length;
     }
 }
