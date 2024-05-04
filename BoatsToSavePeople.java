638845fe30892b73d37f1371516f5c99e38e47b8
Anish Tejwani <91392461+anish891@users.noreply.github.com>
2024-05-04 19:22:41 +0530

    Add files via upload

---
+++ BoatsToSavePeople.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class BoatsToSavePeople {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int count = 0;
+        int i=0, j = people.length-1;
+        while(i<=j){
+            if(people[i] + people[j] <= limit){
+                i++;
+            }
+            j--;
+            count++;
+        }
+        return count;
+    }
+}
